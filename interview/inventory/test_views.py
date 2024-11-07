fb5c90b534076af5e087342b056270917cd33685
Joseph Gilbert <joseph.j.gilbert@gmail.com>
2024-11-07 20:51:44 +0000

    autoset fields can't be set on create but can be updated.

--- interview/inventory/test_views.py
+++ interview/inventory/test_views.py
@@ -3,25 +3,29 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 from interview.inventory.models import Inventory, InventoryLanguage, InventoryType
-from datetime import datetime, timedelta
 from django.utils import timezone
+from datetime import timedelta
 
 class InventoryListByDateViewTest(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.url = reverse('inventory-list-by-date', kwargs={'date': (timezone.now() - timedelta(days=1)).date()})
-        
+        self.recent_days = 4
+
         # Create related objects
         language = InventoryLanguage.objects.create(name='English')
         inventory_type = InventoryType.objects.create(name='Type 1')
-        
+
         # Create test data
-        Inventory.objects.create(name='Item 2', created_at=(timezone.now() - timedelta(days=3)).date(), metadata={}, language=language, type=inventory_type)
-        Inventory.objects.create(name='Item 3', created_at=(timezone.now() - timedelta(days=1)).date(), metadata={}, language=language, type=inventory_type)
-        Inventory.objects.create(name='Item 1', created_at=(timezone.now() - timedelta(days=5)).date(), metadata={}, language=language, type=inventory_type)
+        self.test_objects = [Inventory.objects.create(name=f'Item {i}', metadata={}, language=language, type=inventory_type) for i in range(1, 9)]
+        for i in range(len(self.test_objects)):
+            self.test_objects[i].created_at = (timezone.now() - timedelta(days=i + 1)).astimezone(timezone.utc)
+            self.test_objects[i].save()
+            self.test_objects[i].refresh_from_db()
 
     def test_inventory_list_by_date(self):
-        response = self.client.get(self.url)
+        url = reverse('inventory-list-by-date', kwargs={'date': (timezone.now() - timedelta(days=self.recent_days)).astimezone(timezone.utc).date()})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['name'], 'Item 3')
+        self.assertEqual(len(response.data), self.recent_days)
+        self.assertEqual(response.data[-1]['name'], f'Item {self.recent_days}')
+
