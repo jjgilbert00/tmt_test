72be17e20bd10febcce519566b7a5eea3b9ccfb3
Joe Gilbert <joseph.j.gilbert@gmail.com>
2024-11-16 23:01:15 +0000

    Email login. Inventory & Order admin sections.

--- interview/profiles/admin.py
+++ interview/profiles/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import UserProfile
 
-admin.site.register(UserProfile, UserAdmin)
 
 # Register your models here.
+admin.site.register(UserProfile, UserAdmin)
